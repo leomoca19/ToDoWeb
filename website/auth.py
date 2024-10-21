bd37f2a41da8ec8a301f625aaf7cca049344dd24
Leonardo_Caceres <leomoca19@gmail.com>
2024-10-20 22:07:22 -0600

    create route placeholders

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,19 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return '<p>Login</>'
+
+
+@auth.route('/logout')
+def logout():
+    return '<p>Logout</p>'
+
+
+@auth.route('/signup')
+def signup():
+    return '<p>Sign Up</p>'
+    
